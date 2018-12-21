74836023bf858c6e2ef92415c9274df071db94c2
DoReVo <DoReVo@github.com>
2018-12-21 19:52:03 +0800

    Cleaned

--- MS/loginTesting/comms.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-  window.addEventListener("storage", function() {
-    for (i = 0; i < sessionStorage.length; i++) {
-      console.log(sessionStorage[i]);
-    }
-  });
-});
