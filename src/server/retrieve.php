fbcdf047d9e83049a00b8137324149c1623e7b53
DoReVo <43557832+DoReVo@users.noreply.github.com>
2019-01-01 17:07:41 +0800

    Retrieve refactoring

--- src/server/retrieve.php
+++ src/server/retrieve.php
@@ -16,40 +16,32 @@
   $clos = mysqli_real_escape_string($con,$_GET['clos']);
   $compl = mysqli_real_escape_string($con,$_GET['comple']);
 
-//   $callArg = mysqli_real_escape_string($con,$_GET['callArg']);
 
   // #warning
   // #spaghettiCodesAhead
 
   // TO decide whether SAP is - or not
-  if($sapC == "No"){
-      $sql= "SELECT * FROM `main` WHERE (`Work Title` LIKE '%$wTitle%' AND `Type 1` LIKE '%$type1%' AND `Type 2` LIKE '%$type2' AND `Description` LIKE '%$desc%'
-      AND `Location` LIKE '%$loca%' AND `Status` LIKE '%$stats%' AND `Company` LIKE '%$comp%' AND `SAP#` = '-' AND `Request By` LIKE '%$reqB%'
-      AND `Request Date` LIKE '%$reqD%' AND `Closed By` LIKE '%$clos%' AND `Completion Date` LIKE '%$compl%' )";
-  }else if($sapC == "Yes"){
-      if($sapB == ""){
-          $sql= "SELECT * FROM `main` WHERE (`Work Title` LIKE '%$wTitle%' AND `Type 1` LIKE '%$type1%' AND `Type 2` LIKE '%$type2' AND `Description` LIKE '%$desc%'
-          AND `Location` LIKE '%$loca%' AND `Status` LIKE '%$stats%' AND `Company` LIKE '%$comp%' AND `SAP#` != '-' AND `Request By` LIKE '%$reqB%'
-          AND `Request Date` LIKE '%$reqD%' AND `Closed By` LIKE '%$clos%' AND `Completion Date` LIKE '%$compl%' )";
-      }else if($sapB != ""){
-          $sql= "SELECT * FROM `main` WHERE (`Work Title` LIKE '%$wTitle%' AND `Type 1` LIKE '%$type1%' AND `Type 2` LIKE '%$type2' AND `Description` LIKE '%$desc%'
-          AND `Location` LIKE '%$loca%' AND `Status` LIKE '%$stats%' AND `Company` LIKE '%$comp%' AND `SAP#` LIKE '%$sapB%' AND `Request By` LIKE '%$reqB%'
-          AND `Request Date` LIKE '%$reqD%' AND `Closed By` LIKE '%$clos%' AND `Completion Date` LIKE '%$compl%' )";
-      }
-      
-  }else if($sapC == ""){
-      $sql= "SELECT * FROM `main` WHERE (`Work Title` LIKE '%$wTitle%' AND `Type 1` LIKE '%$type1%' AND `Type 2` LIKE '%$type2' AND `Description` LIKE '%$desc%'
-      AND `Location` LIKE '%$loca%' AND `Status` LIKE '%$stats%' AND `Company` LIKE '%$comp%' AND `Request By` LIKE '%$reqB%'
-      AND `Request Date` LIKE '%$reqD%' AND `Closed By` LIKE '%$clos%' AND `Completion Date` LIKE '%$compl%' )";
-  }
+
+    switch($sapC){
+        case "Yes":
+            if($sapB==""){
+                $sapQ="`SAP#` != '-'";
+            }
+            else{
+                $sapQ="`SAP#` LIKE '%$sapB%'";
+            }
+            break;
+        case "No":
+        $sapQ="`SAP#` = '-'";
+        break;
+        default:$sapQ="`SAP#` LIKE '%'";
+    }
 
 
-  // $sql="SELECT * FROM `main` WHERE (`Work Title` Like '%$wTitle%' AND `Type 1` Like '%$type1%' AND `Type 2` Like '%$type2%' AND 
-  // `Description` Like '%$desc%' AND `Location` Like '%$loca%' AND `Company` Like '%$comp%' And `%SAP#%` Like '%$sapB%') ";
+$sql= "SELECT * FROM `main` WHERE (`Work Title` LIKE '%$wTitle%' AND `Type 1` LIKE '%$type1%' AND `Type 2` LIKE '%$type2' AND `Description` LIKE '%$desc%'
+AND `Location` LIKE '%$loca%' AND `Status` LIKE '%$stats%' AND `Company` LIKE '%$comp%' AND $sapQ AND `Request By` LIKE '%$reqB%'
+AND `Request Date` LIKE '%$reqD%' AND `Closed By` LIKE '%$clos%' AND `Completion Date` LIKE '%$compl%' )";
 
-
-
-  // CHANGE SQL TO REFLECT WHAT USER INPUT RATHER THAN RETRIEVE ALL
 
   $result=mysqli_query($con,$sql);    
       
