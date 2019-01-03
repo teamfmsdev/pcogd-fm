c6692904451570c82d408e720a22c8c495bde993
DoReVo <43557832+DoReVo@users.noreply.github.com>
2019-01-03 16:18:48 +0800

    Retrieve PDO success

--- src/docReady.js
+++ src/docReady.js
@@ -7,31 +7,7 @@ $(document).ready(function() {
     visibility: "hidden"
   });
 
-  // changeAction();
-
-  if ($("#editButton").css("display") == "block") {
-    $("#editButton").hide("slow");
-  }
-  if ($("#searchButton").css("display") == "block") {
-    $("#searchButton").hide("slow");
-  }
-  if ($("#deleteButton").css("display") == "block") {
-    $("#deleteButton").hide("slow");
-  }
-  if ($("#sapChoice").css("display") == "block") {
-    $("#sapChoice").hide("slow", function() {
-      $("#sapBox").css({
-        // right: "100px"
-        // width: "194px"
-        // gridRow: "1/1",
-        gridColumn: "4/6"
-      });
-    });
-  }
-
-  $("#sapChoice").change(function() {
-    sapChoiceChange();
-  });
+  changeAction();
 
   // document.getElementById("requestdateBox").value = new Date().toDateInputValue();
 });
--- src/interactionMethods.js
+++ src/interactionMethods.js
@@ -5,107 +5,39 @@ function changeAction() {
 
   // openDevArea();
 
-  // Check if checkbox(New or Update Toggle) is ticked , and will show only required buttons for each state.
+  /* Check if checkbox(New or Update Toggle) is ticked ,
+  and will show only required buttons for each state. */
 
-  // If checkbox is UPDATE
-  if (checkBox.prop("checked") == true) {
-    // Global var
-    editArg = 1;
-
-    // Show edit and search button
-    if ($("#editButton").css("display") == "none") {
-      $("#editButton").show("slow");
-    }
-    if ($("#searchButton").css("display") == "none") {
-      $("#searchButton").show("slow");
-    }
-    // Hide save button
-    if ($("#saveButton").css("display") == "block") {
-      $("#saveButton").hide("slow");
-    }
-    // Show SAP Choices box
-    if ($("#sapChoice").css("display") == "none") {
-      $("#sapBox").animate({ width: "94px" }, function() {
-        $("#sapChoice").show("slow");
-        $("#sapBox").css({ gridColumn: "5/5" });
+  switch (checkBox.prop("checked")) {
+    // If Mode is "Update"
+    case true:
+      // Global var
+      editArg = 1;
+      uiControlUpdateMode();
+      Reset();
+      // Enable sapChoice onChange for sapBox enable/disable
+      $("#sapChoice").change(function() {
+        sapChoiceChange();
       });
-    }
-
-    // Set status and request date textbox string to empty
-    $("#statusBox").val("");
-    $("#requestdateBox").val("");
-
-    // Show Line 7 (Closed by Line)
-    $(".closedByLine")
-      .css({ visibility: "visible" })
-      .animate({ opacity: 1.0 }, "slow");
-
-    // Call reset function to empty the form
-    Reset();
-
-    // Set SAP# textbox string to display "-"
-    $("#sapChoice").prop("selectedIndex", 0);
-
-    // Remove statusBox attached event for "NEW" mode
-    $("#statusBox").off();
-  }
-  // If checkbox is NEW
-  else {
-    // Global var
-    editArg = 0;
-
-    // Hide edit, search and delete button
-    if ($("#editButton").css("display") == "block") {
-      $("#editButton").hide("slow");
-    }
-    if ($("#searchButton").css("display") == "block") {
-      $("#searchButton").hide("slow");
-    }
-    if ($("#deleteButton").css("display") == "block") {
-      $("#deleteButton").hide("slow");
-    }
-
-    // Show save button
-    if ($("#saveButton").css("display") == "none") {
-      $("#saveButton").show("slow");
-    }
-    // Hide SAP choices box
-    if ($("#sapChoice").css("display") == "block") {
-      $("#sapChoice").hide("slow", function() {
-        $("#sapBox").css({ gridColumn: "4/5" });
-        $("#sapBox").animate({
-          width: "194px"
-        });
-        $("#sapBox").attr("disabled", false);
+      // Disable statusBox onchange for displaying Line 7
+      $("#statusBox").off();
+      break;
+    // If Mode is "New"
+    case false:
+      // Global var
+      editArg = 0;
+      uiControlNewMode();
+      Reset();
+      setDefaultDate();
+      // Disable sapChoice onChange for sapBox enable/disable
+      $("#sapChoice").off();
+      // Set statusBox onchange for displaying Line 7
+      $("#statusBox").change(function() {
+        statusBoxChange();
       });
-    }
-
-    // Hide Line 7
-    $(".closedByLine").animate({ opacity: 0 }, "slow", function() {
-      $(".closedByLine").css({ visibility: "hidden" });
-    });
-
-    // Empty Line 7 input form
-    $("#closedbyBox").val("");
-    $("#completiondateBox").val("");
-
-    // Set status to "new"
-    Reset();
-    $("#statusBox").val("New");
-    setDefaultDate();
-    $("#sapBox").val("-");
-
-    // Enabled request by box
-    $("#requestbyBox").prop("disabled", false);
-    // $("#requestbyBox").css({"backgroundColor":"rgb(100, 98, 98)"});
-
-    // Enabled request date box
-    $("#requestdateBox").prop("readonly", false);
-    // $("#requestdateBox").css({"backgroundColor":"rgb(100, 98, 98)"});
-
-    $("#statusBox").change(function() {
-      statusBoxChange();
-    });
+      break;
+    default:
+      console.log("changeAction error");
   }
 }
 
@@ -140,80 +72,15 @@ function helpAreaToggle() {
   $("#infoArea").toggle("slow");
 }
 
-// Populate input form
-function populateForm(elem) {
-  // var table=document.getElementById("outputTable");
-  // var alertMsg = document.getElementById("alertMsg");
-  // var msg ="";
-
-  var dataBoxes = {
-    1: $("#wTitleBox"), //0
-    2: $("#type1Box"), //1
-    3: $("#type2Box"), //2
-    4: $("#descriptionBox"), //3
-    5: $("#locationBox"), //4
-    6: $("#statusBox"), //5
-    7: $("#companyBox"), //6
-    8: $("#sapBox"), //7
-    9: $("#requestbyBox"), //8
-    10: $("#requestdateBox"), //9
-    11: $("#closedbyBox"), //10
-    12: $("#completiondateBox") // 11
-  };
-  var dropDownBox = {
-    2: "#type1Box",
-    3: "#type2Box",
-    6: "#statusBox",
-    9: "#requestbyBox",
-    11: "#closedbyBox"
-  };
-
-  var postedElemData = [];
-
-  for (i = 0; i < 13; i++) {
-    postedElemData[i] = elem.childNodes[i].innerText;
-    postedElemData[i] = myTrim(postedElemData[i]);
-  }
-  // dataBoxes[9].val(postedElemData[y]);
-  //Fill input boxes with selected row data
-  for (y = 1; y < 13; y++) {
-    switch (y) {
-      case 2:
-
-      case 3:
-
-      case 6:
-
-      case 9:
-        // $(dropDownBox[y]).val(postedElemData[y]);
-        $(dropDownBox[y] + ' option[value="' + postedElemData[y] + '"]').prop(
-          "selected",
-          true
-        );
-        break;
-      case 11:
-        if (postedElemData == "") {
-          continue;
-        }
-
-      default:
-        dataBoxes[y].val(postedElemData[y]);
-    }
-  }
-
-  $("#sapChoice").prop("selectedIndex", 0);
-
-  // Make requestby and date Readonly
-  $("#sapChoice").prop("disabled", true);
-  $("#requestbyBox").prop("disabled", true);
-  $("#requestdateBox").prop("readonly", true);
-}
-
+// Determine sapBox disable or enable
 function sapChoiceChange() {
+  // If sapChoice is "YES", Enable sapBox
   if ($("#sapChoice").prop("selectedIndex") == 1) {
     $("#sapBox").val("");
     $("#sapBox").attr("disabled", false);
-  } else {
+  }
+  // If sapChoice is "No" or blank
+  else {
     $("#sapBox").val("");
     $("#sapBox").attr("disabled", true);
   }
@@ -289,6 +156,68 @@ function uiControlDelete() {
     $("#saveButton").hide("slow");
     $("#editButton").show("slow");
   }
+}
+
+function uiControlNewMode() {
+  // Hide edit, search and delete button
+  if ($("#editButton").css("display") == "block") {
+    $("#editButton").hide("slow");
+  }
+  if ($("#searchButton").css("display") == "block") {
+    $("#searchButton").hide("slow");
+  }
+  if ($("#deleteButton").css("display") == "block") {
+    $("#deleteButton").hide("slow");
+  }
+  // Show save button
+  if ($("#saveButton").css("display") == "none") {
+    $("#saveButton").show("slow");
+  } // Hide SAP choices box
+  if ($("#sapChoice").css("display") == "block") {
+    $("#sapChoice").hide("slow", function() {
+      $("#sapBox").css({ gridColumn: "4/5" });
+      $("#sapBox").animate({
+        width: "194px"
+      });
+      $("#sapBox").attr("disabled", false);
+    });
+  }
+  // Hide Line 7
+  $(".closedByLine").animate({ opacity: 0 }, "slow", function() {
+    $(".closedByLine").css({ visibility: "hidden" });
+  });
+  // Enabled request by box
+  $("#requestbyBox").prop("disabled", false);
+  // $("#requestbyBox").css({"backgroundColor":"rgb(100, 98, 98)"});
+
+  // Enabled request date box
+  $("#requestdateBox").prop("readonly", false);
+  // $("#requestdateBox").css({"backgroundColor":"rgb(100, 98, 98)"});
+}
+
+function uiControlUpdateMode() {
+  // Show edit and search button
+  if ($("#editButton").css("display") == "none") {
+    $("#editButton").show("slow");
+  }
+  if ($("#searchButton").css("display") == "none") {
+    $("#searchButton").show("slow");
+  }
+  // Hide save button
+  if ($("#saveButton").css("display") == "block") {
+    $("#saveButton").hide("slow");
+  }
+  // Show SAP Choices box
+  if ($("#sapChoice").css("display") == "none") {
+    $("#sapBox").animate({ width: "94px" }, function() {
+      $("#sapChoice").show("slow");
+      $("#sapBox").css({ gridColumn: "5/5" });
+    });
+  }
+  // Show Line 7 (Closed by Line)
+  $(".closedByLine")
+    .css({ visibility: "visible" })
+    .animate({ opacity: 1.0 }, "slow");
 }
 
 // Depreciated hide/show toggle
