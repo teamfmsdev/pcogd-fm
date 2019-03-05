840bc175024a8c31c4083ed83aeac677063144e1
DoReVo <DoReVo@github.com>
2019-03-05 19:07:48 +0800

    Added store getters,state,actions for form

--- reWritesVue/src/store.js
+++ reWritesVue/src/store.js
@@ -1,16 +1,54 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import dayjs from 'dayjs'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    formState: false,
+    formData: {
+      wTitle: '',
+      prio: '',
+      t1: '',
+      t2: '',
+      desc: '',
+      loca: '',
+      stat: '',
+      comp: '',
+      sapS: '',
+      sapN: '',
+      reqBy: '',
+      reqD: dayjs(new Date()).format('YYYY-MM-DD'),
+      closBy: '',
+      closD: ''
+    }
 
   },
+  getters: {
+    getFormData: (state) => {
+      return state.formData
+    },
+    getFormState: (state) => {
+      return state.formState
+    }
+  },
   mutations: {
+    changeFormState (state, payload) {
+      state.formState = payload
+    },
+    updateFormData (state, { field, data }) {
+      state.formData[field] = data
+    }
 
   },
   actions: {
-
+    changeFormState ({ commit }, payload) {
+      commit('changeFormState', payload)
+    },
+    updateFormData ({ commit }, payload) {
+      payload.data = payload.data.trim()
+      commit('updateFormData', payload)
+    }
   }
 })
