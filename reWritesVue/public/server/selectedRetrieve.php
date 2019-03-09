2368f2a7b60a68957c5e1e308aaac3a6474e681f
DoReVo <DoReVo@github.com>
2019-03-09 08:28:18 +0800

    Modified selected retrieved to remove nl2br function

--- reWritesVue/public/server/selectedRetrieve.php
+++ reWritesVue/public/server/selectedRetrieve.php
@@ -3,7 +3,11 @@
 require "connection.php";
 
 // Retrive data base on ID from $_GET
-$stmt =$con->prepare("SELECT * FROM `main` WHERE(`row`=?)") ;
+$stmt =$con->prepare("SELECT `row` as fmNo,`Work Title` as wTitle,
+ `Priority` as prio,`Type 1` as t1,`Type 2` as t2,`Description` as 'desc',
+ `Location` as loca,`Status` as stat,`Company` as comp,`SAP#` as sapN,
+ `Request By` as reqBy,`Request Date` as reqD,`Closed By` as closBy,
+ `Completion Date` as closD FROM `main` WHERE(`row`=?)") ;
 
 $stmt -> bindValue(1,$_GET["dataId"]);
 
@@ -11,9 +15,9 @@ $stmt ->execute();
 
 $row=$stmt->fetch(PDO::FETCH_ASSOC);
 
-foreach ($row as $key => $value) {
-    $row[$key] = nl2br($value);
-}
+// foreach ($row as $key => $value) {
+//     $row[$key] = nl2br($value);
+// }
 
 echo json_encode($row);
 
